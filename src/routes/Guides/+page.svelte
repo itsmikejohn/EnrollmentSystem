34876d85a754debc9f9d3bbfbe1c44fd12b71c56
itsmikejohn <eviotamikejohnb@gmail.com>
2023-09-03 21:38:15 +0800

    sample

--- src/routes/Guides/+page.svelte
+++ src/routes/Guides/+page.svelte
@@ -5,7 +5,9 @@
 </script>
 
 <main class="flex flex-col gap-2 text-center" in:scale>
-    <p>This system is forsale @ 3k PHP email me at eviotamikejohnb@gmail.com or chat me direclty on facebook
+    <p>Everything you see here is sample only and it is not connected to database yet.</p>
+
+    <p>This fullstack system is forsale @ 3k PHP email me at eviotamikejohnb@gmail.com or chat me direclty on facebook
 
         <b class="text-blue-500 underline cursor-pointer" on:click={() => window.open("https://www.facebook.com/mike.eviota/")}
         role="button" tabindex="0"
@@ -15,5 +17,7 @@
     </p>
 
     <p class="text-red-500">Additional charge if you want custom functionality that is not included in the system</p>
+
+    <p>Stacks use: SvelteKit, TailwindCSS, Firebase noSQL or Supabase SQL</p>
     
 </main>
